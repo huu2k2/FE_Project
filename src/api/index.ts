9b6b04f2a3592eb76f16205098574c2a79317b46
huu77 <thuu28052002@gmail.com>
2024-11-04 22:25:06 +0700

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
 
 // Define custom response type
 interface CustomResponse<T = any> {
