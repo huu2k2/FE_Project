df00bff34e77ae29976fb3bd8680289dde7add92
huu77 <thuu28052002@gmail.com>
2024-11-05 23:31:30 +0700

    fix: change router

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,12 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import RouterPage404 from "./page_404/router";
 import RouterHomePage from "./home/router";
 import RouterLoginPage from "./login/router";
 import RouterKitchenPage from "./kitchen/router";
-
+ 
 export const routers = createBrowserRouter([
   RouterPage404,
   RouterHomePage,
-  RouterLoginPage,
+  ...RouterLoginPage,
   RouterKitchenPage,
 ]);
