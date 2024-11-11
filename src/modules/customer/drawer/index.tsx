76cdb7b77b848fe501d2f3b131ca9a021a81fa68
Ckt2002 <toan652002@gmail.com>
2024-11-11 14:12:09 +0700

    Commit drawer

--- src/modules/customer/drawer/index.tsx
+++ src/modules/customer/drawer/index.tsx
@@ -6,14 +6,54 @@ export const Drawer: React.FC = () => {
           htmlFor="my-drawer"
           aria-label="close sidebar"
           className="drawer-overlay"></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul>
+        <div className="flex flex-col min-h-full w-80 bg-backgroundColor rounded-r-[30px]">
+          {/* Profile Section */}
+          <div className="flex flex-col items-center p-4 bg-backgroundColor rounded-r-[30px]">
+            <div className="w-24 h-24 rounded-full bg-gray-300"></div>
+            <h2 className="mt-2 font-semibold text-white">Bùi Quốc Triệu</h2>
+          </div>
+
+          {/* Menu Items */}
+          <div className="flex-grow bg-white w-full">
+            <ul className="menu flex flex-col text-black text-content p-4 space-y-2">
+              <li className="flex items-left gap-3 p-2 border-b border-gray-300">
+                <a href={`/person`}>
+                  <i className="fas fa-user"></i>Hồ sơ
+                </a>
+              </li>
+              <li className="flex items-left gap-3 p-2 border-b border-gray-300">
+                <a href={`/order/history`}>
+                  <i className="fas fa-history"></i>Lịch sử gọi món
+                </a>
+              </li>
+              <li className="flex items-left gap-3 p-2 border-b border-gray-300">
+                {/* Change id */}
+                <a href={`/payment/order/aaaaaaa`}>
+                  <i className="fas fa-credit-card"></i>Thanh toán
+                </a>
+              </li>
+              <li className="flex items-left gap-3 p-2 border-b border-gray-300">
+                <a href={`/order/status`}>
+                  <i className="fas fa-shopping-cart"></i>Trạng thái đơn hiện
+                  tại
+                </a>
+              </li>
+              <li className="flex items-left gap-3 p-2 border-b border-gray-300">
+                {/* Send notification for staff */}
+                <a>
+                  <i className="fas fa-table"></i>Gộp bàn
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Logout Button */}
+          <div className="p-4 bg-backgroundColor text-center rounded-r-[30px]">
+            <button className="bg-white px-4 py-2 text-[#ffaa02] rounded-full font-bold w-full">
+              Đăng xuất
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
