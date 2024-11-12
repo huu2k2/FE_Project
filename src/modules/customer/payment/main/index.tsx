146e1c29c524673e7e63309cf7e7ba3405856261
Ckt2002 <toan652002@gmail.com>
2024-11-12 15:56:45 +0700

    commit payment

--- src/modules/customer/payment/main/index.tsx
+++ src/modules/customer/payment/main/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { CustomerHeader } from "../../../../components/CustomerHeader";
 import { FinishOrderItem } from "../../../../components/FinishOrderItem";
 
@@ -52,94 +53,32 @@ export const Payment: React.FC = () => {
       price: 3,
       quantity: 3,
     },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
-    {
-      id: "asdasd",
-      name: "Món đã xác nhận",
-      price: 3,
-      quantity: 3,
-    },
   ];
+
+  const [paymentMethod, setPaymentMethod] = useState("Tiền mặt");
+
+  const handlePaymentMethodChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setPaymentMethod(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const tableInfo = {
+      id: "01", // giả định thông tin bàn
+      name: "Bàn O1",
+      area: "Khu vực A",
+    };
+    console.log("Thông tin bàn:", tableInfo);
+    console.log("Hình thức thanh toán:", paymentMethod);
+  };
 
   return (
     <>
       <CustomerHeader
         isBack={true}
         title="Bàn A02 - 27/10/2024"
-        bg="white"
-      ></CustomerHeader>
+        bg="white"></CustomerHeader>
       <div className="flex flex-1 flex-col mt-4">
         {data.map((item, index) => (
           <FinishOrderItem key={index} data={item}></FinishOrderItem>
@@ -151,7 +90,10 @@ export const Payment: React.FC = () => {
           <p className="text-backgroundColor text-2xl font-bold">
             Loại thanh toán:
           </p>
-          <select className="flex-1 ml-4 p-2 rounded-lg bg-backgroundColor text-white">
+          <select
+            value={paymentMethod}
+            onChange={handlePaymentMethodChange}
+            className="flex-1 ml-4 p-2 rounded-lg bg-backgroundColor text-white">
             <option>Tiền mặt</option>
             <option>Chuyển khoản</option>
           </select>
@@ -161,7 +103,10 @@ export const Payment: React.FC = () => {
           <div className="flex-1 bg-backgroundColor text-white text-1xl p-4 rounded-l-md text-start h-full">
             Tổng tiền: 27000đ
           </div>
-          <button className="flex-1 bg-[#FFAA02] opacity-70 text-white text-1xl p-4 rounded-r-md hover:opacity-100 hover:bg-backgroundColor transition">
+          <button
+            onClick={handleSubmit}
+            className="flex-1 bg-[#FFAA02] opacity-70 text-white text-1xl p-4 rounded-r-md hover:opacity-100 
+          hover:bg-backgroundColor transition">
             Gửi yêu cầu
           </button>
         </div>
