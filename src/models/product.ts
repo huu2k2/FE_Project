afa6c35067e8db4b9bad4a27d13dfe64cc66bff9
huu77 <thuu28052002@gmail.com>
2024-11-16 17:38:18 +0700

    update product

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,8 +1,14 @@
 export interface ProductModel {
+  productId:string;
   image:string;
   name: string;
+  description: string,
   price: string;
-  type:string;
+  categoryId:string;
+  category:{
+    categoryId:string;
+    name:string;
+  }
 }
 
 export interface ProductQuery {
