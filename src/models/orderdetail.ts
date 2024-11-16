5a42896e8d5d175e0dbb32e29dd20084202fa806
trieuquocbui <buiquoctrieu020402@gmail.com>
2024-11-16 10:28:43 +0700

    update

--- src/models/orderdetail.ts
+++ src/models/orderdetail.ts
@@ -1,14 +1,14 @@
 import { OrderDetailStatus } from "../enum/enum";
 import { ProductModel } from "./product";
 
-export class OrderDetailModel {
-  price!: number;
-  product?: ProductModel;
-  orderDetailId?: string;
-  orderId!: string;
-  productId!: string;
-  quantity!: number;
-  status!: OrderDetailStatus;
-  createdAt?: Date;
-  updatedAt?: Date;
+export interface OrderDetailModel {
+  price: number;
+  product: ProductModel;
+  orderDetailId: string;
+  orderId: string;
+  productId: string;
+  quantity: number;
+  status: OrderDetailStatus;
+  createdAt: Date;
+  updatedAt: Date;
 }
