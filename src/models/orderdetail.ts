42b3548a8c16a0bf63891bdca1edaaa60c90a9a2
trieuquocbui <buiquoctrieu020402@gmail.com>
2024-11-16 09:23:51 +0700

    update

--- src/models/orderdetail.ts
+++ src/models/orderdetail.ts
@@ -1,9 +1,6 @@
 import { OrderDetailStatus } from "../enum/enum";
-import { ProductModel } from "./product";
 
 export interface OrderDetailModel {
-  price: number;
-  product: ProductModel;
   orderDetailId: string;
   orderId: string;
   productId: string;
