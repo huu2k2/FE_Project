53a656e131883f8459ea2e09aee3bd533c18cbef
Ckt2002 <toan652002@gmail.com>
2024-11-18 22:19:02 +0700

    commit kitchen

--- src/models/tableDetail.ts
+++ src/models/tableDetail.ts
@@ -1,3 +1,5 @@
+import { TableModel } from "./table";
+
 export class TableDetailModel {
   tableDetailId!: string;
   tableId!: string;
@@ -7,4 +9,16 @@ export class TableDetailModel {
   endTime?: Date | null;
   createdAt!: Date;
   updatedAt?: Date | null;
+}
+
+export class TableDetailModelSocket {
+  tableDetailId!: string;
+  tableId!: string;
+  orderId!: string;
+  note?: string | null;
+  startTime!: Date;
+  endTime?: Date | null;
+  createdAt!: Date;
+  updatedAt?: Date | null;
+  table!: TableModel;
 }
